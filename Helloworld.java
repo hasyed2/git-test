bdfa46c569dafa6b29e58d29641cf299e164814d
Hasan <syedha@mpac.ca>
2021-05-10 15:03:00 -0400

    Latest commit includes java file

---
+++ Helloworld.java
@@ -0,0 +1,9 @@
+public class Helloworld {
+    public static void main(String[] args){
+        System.out.print("Hello World");
+        for (int c=0; c<99; c++)
+            System.out.println((Math.random())*101);
+    }
+
+}
+
